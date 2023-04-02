c895757eceee327ef00a2422df83a6789fe16956
Sebastian Pölsterl <sebp@k-d-w.org>
2023-04-02 21:19:29 +0200

    Skip mkl_pardiso_tests on macOS with Python 3.11

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+test_methods="not codegen"
+if [ "${MACOSX_DEPLOYMENT_TARGET:-x}" != "x" -a "${STDLIB_DIR%3.11}" != "${STDLIB_DIR}" ]; then
+  test_methods="${test_methods} and not mkl_pardiso_tests"
+fi
 tests_path=$($PYTHON -c "from os.path import join; import osqp; print(join(osqp.__path__[0], 'tests'))")
-$PYTHON -m pytest "${tests_path}" -k "not codegen"
+$PYTHON -m pytest "${tests_path}" -k "${test_methods}"
