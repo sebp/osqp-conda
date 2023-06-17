8d8908823a1121c348fbcf8e2ae32d66e38011a8
Sebastian Pölsterl <sebp@k-d-w.org>
2023-06-17 16:18:20 +0200

    Skip mkl_paradio_tests if numpy does not support MKL

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,7 +2,8 @@
 set -e
 
 test_methods="not codegen"
-if [ "${MACOSX_DEPLOYMENT_TARGET:-x}" != "x" -a "${STDLIB_DIR%3.11}" != "${STDLIB_DIR}" ]; then
+mkl_info=$(python -c 'import numpy; numpy.show_config()' | grep -A1 'blas_mkl_info' | tail -n 1 | tr -d ' ')
+if [ "${MACOSX_DEPLOYMENT_TARGET:-x}" != "x" -a "${mkl_info}" = "NOTAVAILABLE" ]; then
   test_methods="${test_methods} and not mkl_pardiso_tests"
 fi
 tests_path=$($PYTHON -c "from os.path import join; import osqp; print(join(osqp.__path__[0], 'tests'))")
