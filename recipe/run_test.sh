31a08a381ab25a1512d379ecd8151f008029c064
Sebastian Pölsterl <sebp@k-d-w.org>
2023-11-28 19:29:53 +0100

    Fix checking for numpy with MKL support
    The output of numpy.show_config() changed in numpy 1.25

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,7 +2,16 @@
 set -e
 
 test_methods="not codegen"
-mkl_info=$(python -c 'import numpy; numpy.show_config()' | grep -A1 'blas_mkl_info' | tail -n 1 | tr -d ' ')
+
+# the output of show_config() changes in 1.25
+old_show_config=$(python -c 'from importlib.metadata import version; from packaging.version import Version, parse; print(parse(version("numpy")) < parse("1.25.0"))')
+
+if [ "${old_show_config}" = "True" ]; then
+  mkl_info=$($PYTHON -c 'import numpy; numpy.show_config()' | grep -A1 'blas_mkl_info' | tail -n 1 | tr -d ' ')
+else
+  mkl_info=$($PYTHON -c 'import numpy; numpy.show_config()' | grep -m 1 'name: mkl' || echo 'NOTAVAILABLE')
+fi
+
 if [ "${MACOSX_DEPLOYMENT_TARGET:-x}" != "x" -a "${mkl_info}" = "NOTAVAILABLE" ]; then
   test_methods="${test_methods} and not mkl_pardiso_tests"
 fi
